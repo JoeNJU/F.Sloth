b72ece261a1cc5f4e24b2ce85af57f45d4025f1b
JoeNJU <joedawson98@yahoo.com>
2016-04-01 17:15:05 +0800

    Revert "Revert "Added files via upload""
    This reverts commit db944583ca5d4b82b9a515592fb5ac1c098e129a.

---
+++ JPanelStart.java
@@ -0,0 +1,50 @@
+package ui;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class JPanelStart extends JPanel{
+	
+	private static final long serialVersionUID = 1L;
+	/*本类用于游戏开头动画的制作
+	 * 不是首要任务
+	 */
+	
+	private JMainFrame jMainFrame;
+
+	
+	public JPanelStart(JMainFrame jframe){
+		this.jMainFrame = jframe;
+		this.setLayout(null);
+		this.setBounds(0, 0, JMainFrame.JFRAME_WIDTH, JMainFrame.JFRAME_HIGHT);
+		this.setVisible(true);
+		jMainFrame.setDragable(this);
+		this.initial();
+	}
+	
+	public void paintComponent(Graphics g){
+
+		g.drawRect(0,0,JMainFrame.JFRAME_WIDTH, JMainFrame.JFRAME_HIGHT);
+	
+	}
+	
+	public void initial(){
+		
+	}
+	
+	
+	
+	
+	class ButtonListener implements ActionListener{
+		
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+
+		}
+			
+	}
+
+}
