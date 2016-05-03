8557b0cdc9bfde79b0721cbc839efdb6dd4a9870
VivianNJU <1916496828@qq.com>
2016-05-03 22:51:00 +0800

    Added files via upload

---
+++ JPanelGame.java
@@ -0,0 +1,144 @@
+package ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import game.Control;
+import game.Person;
+
+
+public class JPanelGame extends JPanel{
+	private static final long serialVersionUID = 1L;
+	/*本类用于游戏的主界面
+	 * 
+	 */
+	
+	private JMainFrame jMainFrame;
+	private JButton[] buttons;
+	@SuppressWarnings("unused")
+	private Control control;
+	
+	
+	public JPanelGame(JMainFrame jframe){
+		this.jMainFrame = jframe;
+		
+		
+		this.setLayout(null);
+		this.setBounds(0, 0, JMainFrame.JFRAME_WIDTH, JMainFrame.JFRAME_HIGHT);
+		this.setVisible(true);
+		jMainFrame.setDragable(this);
+		jMainFrame.setContentPane(this);
+		initialButtons();
+		control = new Control(this);
+		this.repaint();
+	}
+	
+	public void initialButtons(){
+		this.buttons=new JButton[]{new JButton(),new JButton(),new JButton(),new JButton(),new JButton(),new JButton(),new JButton(),new JButton()};
+		//Point[] points=new Point[]{new Point(100,200),new Point(200,200),new Point(300,200),new Point(400,200)};
+		for (int i = 0; i < 4; i++) {
+			//buttons[i].setBounds(points[i].x, points[i].y, 80,50);
+			buttons[i].setBorderPainted(false);
+			this.add(buttons[i]);
+			buttons[i].setIcon(jMainFrame.everyImage.IMG_GAME_BUTTONS[i]);
+			buttons[i].setBorderPainted(false);
+			buttons[i].setContentAreaFilled(false);
+			ButtonListener listener=new ButtonListener(i);
+			buttons[i].addMouseListener(listener);
+			buttons[i].addActionListener(listener);
+			buttons[i].setVisible(false);
+		} 
+	}
+	
+	public void basicAdd(Person person){//添加基础选项，x和y为人物所在 格子坐标
+		Point location = person.getRealPoint();//真实坐标
+		Point[] points=new Point[]{new Point(location.x+20,location.y-50),new Point(location.x-33,location.y-35),new Point(location.x-55,location.y+10),new Point(location.x-33,location.y+58)};
+		if(person.isHide()){
+			buttons[3].setIcon(jMainFrame.everyImage.IMG_GAME_BUTTONS[4]);
+			buttons[3].setBounds(points[0].x,points[0].y,40,40);
+			buttons[3].setVisible(true);
+			if(person.getActivity()!=1){
+				buttons[2].setBounds(points[1].x,points[1].y,40,40);
+				buttons[2].setVisible(true);
+			}			
+		}else if(person.getActivity()<4){
+			buttons[3].setIcon(jMainFrame.everyImage.IMG_GAME_BUTTONS[3]);
+			for(int i = 3;i>3-person.getActivity();i--){
+				buttons[i].setBounds(points[i].x,points[i].y,40,40);
+				buttons[i].setVisible(true);
+			}
+			
+		}else{
+			buttons[3].setIcon(jMainFrame.everyImage.IMG_GAME_BUTTONS[3]);
+			for(int i = 0;i<4;i++){
+				buttons[i].setBounds(points[i].x,points[i].y,40,40);
+				buttons[i].setVisible(true);
+			}
+		}
+		this.repaint();
+	}
+	
+	public void removeButtons(){//去掉屏幕上已经显示的按钮
+		for(int i = 0;i<4;i++){
+			buttons[i].setVisible(false);
+		}
+	}
+	
+	public void directAdd(){//添加上下左右按钮
+		
+	}
+	
+	public void paintComponent(Graphics g){
+		g.drawImage(jMainFrame.everyImage.IMG_GAME_BG, 0, 0,JMainFrame.JFRAME_WIDTH, JMainFrame.JFRAME_HIGHT,null);
+	
+	}
+	
+	class ButtonListener extends MouseAdapter implements ActionListener{
+		int i;
+		private ButtonListener(int i){
+			this.i=i;
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e){
+			buttons[i].setLocation(buttons[i].getLocation().x+2, buttons[i].getLocation().y-2);
+			/*if(jFrameGame.isVoice()){
+				new AudioPlayer("");
+			}*/
+		}
+		@Override
+		public void mouseExited(MouseEvent e){
+			buttons[i].setLocation(buttons[i].getLocation().x-2, buttons[i].getLocation().y+2);
+			
+		}
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			switch (i) {
+			case 0://占领
+				
+				break;
+			case 1://骚扰
+				
+				break;
+			case 2://移动
+				
+				break;
+			case 3://隐身或现身
+				
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+}
